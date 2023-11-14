8b84687823b7a7c5d7c64f7ba2c1421e095a104c
Pedrooaj <133423641+Pedrooaj@users.noreply.github.com>
2023-11-14 15:57:47 -0300

    Delete Junior/Api-Mongoose directory

--- Junior/Api-Mongoose/src/server.js
+++
@@ -1,5 +0,0 @@
-import app from "./app";
-
-app.listen(5000, ()=> {
-    console.log("API FUNCIONANDO CORRETAMENTE Na PORTA 5000!!")
-});
--- Junior/Api-Mongoose/src/services/auth.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcryptjs";
-import UsersController from "../controllers/UsersController";
-
-export const createPasswordHash = async (password) => {
-   return bcrypt.hash(password, 8);
-}
